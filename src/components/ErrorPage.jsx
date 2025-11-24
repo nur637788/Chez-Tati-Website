a21ccbbccef09b3a2c4bfd20d5e074c566a4fc24
Md Abdun Nur <mdnoyon631234@gmail.com>
2025-11-24 14:46:46 +0600

    Add Fav-icon

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -2,15 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function ErrorPage() {
     return (
-        <>
-            {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full">
-        <body class="h-full">
-        ```
-      */}
+        <> 
             <main className="grid min-h-screen place-items-center bg-gray-900 px-6 py-24 sm:py-32 lg:px-8">
                 <div className="text-center">
                     <p className="text-xl md:text-2xl font-semibold text-indigo-400">404</p>
@@ -18,6 +10,7 @@ export default function ErrorPage() {
                         Page not found
                     </h1>
                     <p className="mt-6 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">
+                     
                         Sorry, we couldn’t find the page you’re looking for.
                     </p>
                     <div className="mt-10 flex items-center justify-center gap-x-6">
